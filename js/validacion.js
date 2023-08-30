c3dee2bbc9c7ffef11958720479d3405d33b558b
Ryomax17 <42577059+Ryomax17@users.noreply.github.com>
2023-08-30 19:08:33 -0300

    lets try

--- js/validacion.js
+++ js/validacion.js
@@ -14,16 +14,20 @@ document.addEventListener("DOMContentLoaded", function(){
     let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
     let usuarioChange = localStorage.getItem("usuarioChange");
     
+    if (usuarioChange !== null) {
+    let cambiarUsuario = document.getElementById("cuenta");
+    cambiarUsuario.innerHTML = usuarioChange;
+    }
+    
     if (!window.location.pathname.includes("login.html")) {
         if (recuerdameValue !== 'true' && !localStorage.getItem("sesionIniciada")) {
             setTimeout(function() {
                 window.location.href = "login.html";
             }, 2000);
-        } else { let cambiarUsuario = document.getElementById("cuenta");
-        cambiarUsuario.innerHTML = usuarioChange;
-    }
+        } 
     } 
-    
+
+
     console.log(usuarioChange);
     console.log(usuarios);
 });
