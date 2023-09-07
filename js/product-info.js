27e6b16d77a9cd5da8afccc6e3cf6a596cf22a33
Romina Gonzalez <rominavgs1996@gmail.com>
2023-09-06 20:26:36 -0400

    Modificacion en proceso

---
+++ js/product-info.js
@@ -0,0 +1,63 @@
+const productInfoDiv = document.getElementById("prod info container");
+
+const productIdInfo = localStorage.getItem("catID");
+
+if (productIdInfo) {
+    // Construye la URL de los productos utilizando el identificador de categoría almacenado.
+    const PRODS_INFO_URL = `https://japceibal.github.io/emercado-api/products/${productIdInfo}.json`;
+  
+
+// Realizar la petición HTTP
+fetch(PRODS_INFO_URL)
+    .then(response => response.json())
+    .then(data => {
+
+            // Manipular los datos recibidos y mostrar en el HTML
+        /* const products = data["products"];
+        for (let i = 0; i < products.length; i++) {
+            const product = products[i]; */
+          
+         /*    const productDiv = document.createElement("div");
+            const productId = document.createElement("p"); //modificacion//
+            idname= "id"+ product.id
+            productDiv.setAttribute("id",idname );
+            productDiv.classList.add("product");
+            const productName = document.createElement("h2");
+            productName.textContent = product.name;
+            productName.setAttribute("class", "textcont");
+            const productImage = document.createElement("img");
+            productImage.src = product.image;
+            productImage.alt = product.name;
+            const textdiv =document.createElement("div");
+            const productPrice = document.createElement("p");
+            productPrice.textContent = `Precio: USD${product.cost}`;
+            productPrice.setAttribute("class","textcont")
+            const productDescription = document.createElement("p");
+            productDescription.textContent = product.description;
+            productDescription.setAttribute("class", "textcont");
+            const productSold = document.createElement("p");
+            productSold.textContent = `Cantidad vendida: ${product.soldCount}`;
+            productSold.setAttribute("class", "productsold");
+            cartbttn = document.createElement("button");
+            cartbttn.innerHTML = "comprar";
+            cartbttn.setAttribute("class", "cartbttn");
+
+            textdiv.appendChild(productName);
+            textdiv.appendChild(productDescription);
+            textdiv.appendChild(productPrice);
+            textdiv.appendChild(cartbttn);
+            productDiv.appendChild(productSold);
+            productDiv.appendChild(productImage);
+            productDiv.appendChild(productId);
+            productDiv.appendChild(textdiv);
+            productListDiv.appendChild(productDiv); */
+        };
+    })
+    .catch(error => {
+        console.error("Error al cargar los productos:", error);
+    });
+
+} else {
+    // Maneja el caso en el que no se haya seleccionado una categoría.
+    console.error("No se ha seleccionado una categoría de productos.");
+  }
