a73dde406a852c2f77228fc48da3c8cdf5d020ae
maximiliano836 <maxi10.02.00.g@gmail.com>
2023-09-08 20:15:45 -0300

    correcion funcion de busqueda por precios

--- js/filtrado_products.js
+++ js/filtrado_products.js
@@ -5,52 +5,63 @@ let currentCategoriesArray = [];
 let currentSortCriteria = undefined;
 let minCount = undefined;
 let maxCount = undefined;
-const ORDER_ASC_BY_PRICE = "1-10"
-const ORDER_DESC_BY_PRICE = "10-1"
+const ORDER_ASC_BY_PRICE = "1-10";
+const ORDER_DESC_BY_PRICE = "10-1";
 let search = document.getElementById("buscador");
-
 
 //Hace lo mismo que sortCategories pero ordena el array por precio
 function sortCategoriesByPrice(criteria, array) {
-    let p_result = [];
-    if (criteria === ORDER_ASC_BY_PRICE) {
-        p_result = array.sort((a, b) => a.cost - b.cost);
-        return p_result;
-    } else if (criteria === ORDER_DESC_BY_PRICE) {
-        p_result = array.sort((a, b) => b.cost - a.cost);
-        return p_result;
-    } else if (criteria === ORDER_BY_PROD_COUNT) {
-        p_result = array.sort((a, b) => b.soldCount - a.soldCount);
-        return p_result;
-    }else{
-        return p_result;
-    }
+  let p_result = [];
+  if (criteria === ORDER_ASC_BY_PRICE) {
+    p_result = array.sort((a, b) => a.cost - b.cost);
+    return p_result;
+  } else if (criteria === ORDER_DESC_BY_PRICE) {
+    p_result = array.sort((a, b) => b.cost - a.cost);
+    return p_result;
+  } else if (criteria === ORDER_BY_PROD_COUNT) {
+    p_result = array.sort((a, b) => b.soldCount - a.soldCount);
+    return p_result;
+  } else {
+    return p_result;
+  }
 }
 
 function filterProductsByPrice() {
-    const minPrice = parseInt(document.getElementById("rangeFilterCountMin").value);
-    const maxPrice = parseInt(document.getElementById("rangeFilterCountMax").value);
-
-    const filteredProducts = currentCategoriesArray.filter(function (product) {
-        return (minPrice === undefined || product.cost >= minPrice) &&
-               (maxPrice === undefined || product.cost <= maxPrice);
-    });
-
-    showCategoriesList(filteredProducts);
-}
+    const minPriceInput = document.getElementById("rangeFilterCountMin");
+    const maxPriceInput = document.getElementById("rangeFilterCountMax");
+  
+    const minPrice = parseInt(minPriceInput.value);
+    const maxPrice = parseInt(maxPriceInput.value);
+  
+    if (isNaN(minPrice) && isNaN(maxPrice)) {
+      showCategoriesList(currentCategoriesArray);
+    } else {
+      const filteredProducts = currentCategoriesArray.filter(function (product) {
+        return (
+          (isNaN(minPrice) || product.cost >= minPrice) &&
+          (isNaN(maxPrice) || product.cost <= maxPrice)
+        );
+      });
+      showCategoriesList(filteredProducts);
+    }
+  }
 
 function setCatID(id) {
-    localStorage.setItem("catID", id);
-    window.location = "products.html";
+  localStorage.setItem("catID", id);
+  window.location = "products.html";
 }
 
 function showCategoriesList(array) {
-    let htmlContentToAppend = "";
-    for (let i = 0; i < array.length; i++) {
-        let category = array[i];
-        if (((minCount == undefined) || (minCount != undefined && parseInt(category.cost) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.cost) <= maxCount))) {
-                htmlContentToAppend += `
+  let htmlContentToAppend = "";
+  for (let i = 0; i < array.length; i++) {
+    let category = array[i];
+    if (
+      (minCount == undefined ||
+        (minCount != undefined && parseInt(category.cost) >= minCount)) &&
+      (maxCount == undefined ||
+        (maxCount != undefined && parseInt(category.cost) <= maxCount))
+    ) {
+      htmlContentToAppend += `
                 <div class="list-group-item list-group-item-action cursor-active" onclick="setCatID(${category.id})">
                     <div class="row">
                         <div class="col-3">
@@ -66,85 +77,89 @@ function showCategoriesList(array) {
                     </div>
                 </div>
             `;
-        }
     }
-    document.getElementById("product-list").innerHTML = htmlContentToAppend;
+  }
+  document.getElementById("product-list").innerHTML = htmlContentToAppend;
 }
 
 //es lo mismo que sort and show categories solo que acomodado a a funcion de asc y desc
 function sortAndShowAscAndDesc(sortCriteria, categoriesArray) {
-    currentSortCriteria = sortCriteria;
-    if (categoriesArray != undefined) {
-        currentCategoriesArray = categoriesArray;
-    }
-    currentCategoriesArray = sortCategoriesByPrice(currentSortCriteria, currentCategoriesArray);
-    showCategoriesList(currentCategoriesArray);
+  currentSortCriteria = sortCriteria;
+  if (categoriesArray != undefined) {
+    currentCategoriesArray = categoriesArray;
+  }
+  currentCategoriesArray = sortCategoriesByPrice(
+    currentSortCriteria,
+    currentCategoriesArray
+  );
+  showCategoriesList(currentCategoriesArray);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const productListDiv = document.getElementById("product-list");
+  const productListDiv = document.getElementById("product-list");
 
-const categoriaId = localStorage.getItem("catID");
+  const categoriaId = localStorage.getItem("catID");
 
-    if (categoriaId) {
-        // Construye la URL de los productos utilizando el identificador de categoría almacenado.
-        const PRODS_URL = `https://japceibal.github.io/emercado-api/cats_products/${categoriaId}.json`;
-        fetch(PRODS_URL)
-            .then(response => response.json())
-            .then(data => {
-                if (data.products) {  // Verificamos que "products" exista en el objeto
-                    currentCategoriesArray = data.products;
-                    showCategoriesList();
-                } else {
-                    console.error("No se encontraron productos en el JSON.");
-                }
-            })
-            .catch(error => console.error('Error fetching JSON:', error));
-
-        document.getElementById("sortAsc").addEventListener("click", function () {
-            sortAndShowAscAndDesc(ORDER_ASC_BY_PRICE);
-        });
-
-        document.getElementById("sortDesc").addEventListener("click", function () {
-            sortAndShowAscAndDesc(ORDER_DESC_BY_PRICE);
-        });
-
-        document.getElementById("sortByCount").addEventListener("click", function () {
-            sortAndShowAscAndDesc(ORDER_BY_PROD_COUNT);
-        });
-
-        document.getElementById("clearRangeFilter").addEventListener("click", function () {
-            document.getElementById("rangeFilterCountMin").value = "";
-            document.getElementById("rangeFilterCountMax").value = "";
-            showCategoriesList(currentCategoriesArray);
-        });
-
-        document.getElementById("rangeFilterCount").addEventListener("click", function () {
-            filterProductsByPrice();
-            showCategoriesList();
-        });
-
-        search.addEventListener("input", e => {
-            
-            let inputText = e.target.value.toLowerCase().trim()//obtiene el valor que tiene el input en cada momento-trim()me elimina los espacios en blanco que pueden haber-tolowercase cambia todo el texto a minuscula cuando es procesado para que me busque lo que quiero este usando mayus o minus.
-            console.log(inputText);
-            let mostrarfiltro = currentCategoriesArray.filter(function (elmnt) {
-                return elmnt.name.toLowerCase().trim().includes(inputText) || elmnt.description.toLowerCase().trim().includes(inputText);
-                
-            })
-            if (mostrarfiltro.length) {
-                showCategoriesList(mostrarfiltro);
-            } else {
-                productListDiv.innerHTML="No hay productos que coincidan con su busqueda"
-            }
+  if (categoriaId) {
+    // Construye la URL de los productos utilizando el identificador de categoría almacenado.
+    const PRODS_URL = `https://japceibal.github.io/emercado-api/cats_products/${categoriaId}.json`;
+    fetch(PRODS_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.products) {
+          // Verificamos que "products" exista en el objeto
+          currentCategoriesArray = data.products;
+          showCategoriesList();
+        } else {
+          console.error("No se encontraron productos en el JSON.");
         }
-        
-    )
-    
-    
-    }
+      })
+      .catch((error) => console.error("Error fetching JSON:", error));
 
+    document.getElementById("sortAsc").addEventListener("click", function () {
+      sortAndShowAscAndDesc(ORDER_ASC_BY_PRICE);
+    });
 
+    document.getElementById("sortDesc").addEventListener("click", function () {
+      sortAndShowAscAndDesc(ORDER_DESC_BY_PRICE);
+    });
+
+    document
+      .getElementById("sortByCount")
+      .addEventListener("click", function () {
+        sortAndShowAscAndDesc(ORDER_BY_PROD_COUNT);
+      });
+
+    document
+      .getElementById("clearRangeFilter")
+      .addEventListener("click", function () {
+        document.getElementById("rangeFilterCountMin").value = "";
+        document.getElementById("rangeFilterCountMax").value = "";
+        showCategoriesList(currentCategoriesArray);
+      });
+
+    document
+      .getElementById("rangeFilterCount")
+      .addEventListener("click", function () {
+        filterProductsByPrice();
+        showCategoriesList();
+      });
+
+    search.addEventListener("input", (e) => {
+      let inputText = e.target.value.toLowerCase().trim(); //obtiene el valor que tiene el input en cada momento-trim()me elimina los espacios en blanco que pueden haber-tolowercase cambia todo el texto a minuscula cuando es procesado para que me busque lo que quiero este usando mayus o minus.
+      console.log(inputText);
+      let mostrarfiltro = currentCategoriesArray.filter(function (elmnt) {
+        return (
+          elmnt.name.toLowerCase().trim().includes(inputText) ||
+          elmnt.description.toLowerCase().trim().includes(inputText)
+        );
+      });
+      if (mostrarfiltro.length) {
+        showCategoriesList(mostrarfiltro);
+      } else {
+        productListDiv.innerHTML =
+          "No hay productos que coincidan con su busqueda";
+      }
+    });
+  }
 });
-
-
