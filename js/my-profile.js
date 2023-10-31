b06743ba44469f265dc0ecfcf3ece46427afee4e
LeandroRdeB <140977411+leandrodeborba@users.noreply.github.com>
2023-10-31 16:05:38 -0300

    cargar datos

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,38 +1,71 @@
-window.onload = agregarEmail;
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#btnGuardar').addEventListener('click', guardarDatos);
-});
 
-
-function guardarDatos() {
+window.onload = function () {
+    agregarEmail();
+    agregarDatosValue();
+  };
+  
+  const valueLocal = JSON.parse(localStorage.getItem('datosGuardados')) || {
+    nombre: '',
+    segundoNombre: '',
+    apellido: '',
+    segundoApellido: '',
+    email: '',
+    telefono: ''
+  };
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#btnGuardar').addEventListener('click', function () {
+      guardarDatos();
+      agregarDatosValue();
+    });
+  });
+  
+  function guardarDatos() {
     let nombre = document.getElementById('txtNombre').value;
     let segundoNombre = document.getElementById('txtSegundoNombre').value;
     let apellido = document.getElementById('txtApellido').value;
     let segundoApellido = document.getElementById('txtSegundoApellido').value;
     let email = document.getElementById('txtEmail').value;
     let telefono = document.getElementById('txtTelefono').value;
-
-    const datos = {
-        nombre,
-        segundoNombre,
-        apellido,
-        segundoApellido,
-        email,
-        telefono
-    };
-
-    localStorage.setItem('datosGuardados', JSON.stringify(datos));
-
-    alert('Datos guardados en el localStorage');
-}
-
-
-
-// agregar el email al div 
-function agregarEmail() {
-
-    let usuarioChange = JSON.parse(localStorage.getItem("usuarioChange"));
+  
+    valueLocal.nombre = nombre;
+    valueLocal.segundoNombre = segundoNombre;
+    valueLocal.apellido = apellido;
+    valueLocal.segundoApellido = segundoApellido;
+    valueLocal.email = email;
+    valueLocal.telefono = telefono;
+  
+    localStorage.setItem('datosGuardados', JSON.stringify(valueLocal));
+    alert('Datos guardados');
+  }
+  
+  function agregarDatosValue() {
+    const sFistName = document.getElementById('txtSegundoNombre');
+    sFistName.value = valueLocal.segundoNombre;
+  
+    const lastName = document.getElementById('txtApellido');
+    lastName.value = valueLocal.apellido;
+  
+    const sLastName = document.getElementById('txtSegundoApellido');
+    sLastName.value = valueLocal.segundoApellido;
+  
+    const txtTelNum = document.getElementById('txtTelefono');
+    txtTelNum.value = valueLocal.telefono;
+  
+    const firstName = document.getElementById('txtNombre');
+    firstName.value = valueLocal.nombre;
+  }
+  
+  function agregarEmail() {
+    const datosUsu = localStorage.getItem('usuarioChange');
     const divFormulario = document.getElementById('txtEmail');
-    const nombreGuardado = usuarioChange.email
-    divFormulario.value= nombreGuardado;
-}
+    divFormulario.value = datosUsu.email;
+  }
+  
+  function agregarEmail() {
+    const datosUsu = JSON.parse(localStorage.getItem('usuarioChange'));
+    if (datosUsu) {
+      const divFormulario = document.getElementById('txtEmail');
+      divFormulario.value = datosUsu.email;
+    }
+  }
