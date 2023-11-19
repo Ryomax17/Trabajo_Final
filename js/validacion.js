c5193ef4456ef6eba2c07a0cee8890f0102ac980
maximiliano836 <maxi10.02.00.g@gmail.com>
2023-11-19 13:32:10 -0300

    segundo intento

--- js/validacion.js
+++ js/validacion.js
@@ -1,98 +1,96 @@
 function toggleMode() {
-    const body = document.body
-    body.classList.toggle('day-mode');
-    body.classList.toggle('night-mode');
+  const body = document.body;
+  body.classList.toggle("day-mode");
+  body.classList.toggle("night-mode");
 }
 
-if (localStorage.getItem('claseBody')) {
-    const claseGuardada = localStorage.getItem('claseBody');
-    document.body.className = claseGuardada;
+if (localStorage.getItem("claseBody")) {
+  const claseGuardada = localStorage.getItem("claseBody");
+  document.body.className = claseGuardada;
 }
 
-window.addEventListener('beforeunload', function () {
-    const claseActual = document.body.className;
-    localStorage.setItem('claseBody', claseActual);
+window.addEventListener("beforeunload", function () {
+  const claseActual = document.body.className;
+  localStorage.setItem("claseBody", claseActual);
 });
 
 function getCookie(name) {
-    const cookieArray = document.cookie.split('; ');
-    for (const cookie of cookieArray) {
-        const [cookieName, cookieValue] = cookie.split('=');
-        if (cookieName === name) {
-            return decodeURIComponent(cookieValue);
-        }
+  const cookieArray = document.cookie.split("; ");
+  for (const cookie of cookieArray) {
+    const [cookieName, cookieValue] = cookie.split("=");
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
     }
-    return null;
+  }
+  return null;
 }
 
 function deleteCookie(name) {
-    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  let rememberMeCookie = getCookie("rememberMe");
+  console.log(rememberMeCookie);
+  let loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
 
-    let rememberMeCookie = getCookie('rememberMe');
-    console.log(rememberMeCookie);
-    let loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
+  if (
+    !localStorage.getItem("loggedIn") ||
+    localStorage.getItem("loggedIn") !== "true"
+  ) {
+    if (rememberMeCookie) {
+      let userId = parseInt(rememberMeCookie, 10);
+      console.log(userId);
+      let usersArray = JSON.parse(localStorage.getItem("usersArray")) || [];
 
-
-
-    if ((!localStorage.getItem("loggedIn")) || (localStorage.getItem("loggedIn") !== "true")){
-        if (rememberMeCookie) {
-            let userId = parseInt(rememberMeCookie, 10);
-            console.log(userId);
-            let usersArray = JSON.parse(localStorage.getItem("usersArray")) || [];
-
-            let lastLoggedInUser = usersArray.find(user => user.id === userId);
-            console.log(lastLoggedInUser);
-            if (lastLoggedInUser) {
-                localStorage.setItem("loggedIn", "true");
-                userString = JSON.stringify(lastLoggedInUser);
-                localStorage.setItem("loggedUser", userString);
-                window.location.reload();
-            } else {
-                localStorage.setItem("loggedIn", "false");
-                localStorage.removeItem("loggedUser");
-                window.location.reload();
-            }
-        }
+      let lastLoggedInUser = usersArray.find((user) => user.id === userId);
+      console.log(lastLoggedInUser);
+      if (lastLoggedInUser) {
+        localStorage.setItem("loggedIn", "true");
+        userString = JSON.stringify(lastLoggedInUser);
+        localStorage.setItem("loggedUser", userString);
+        window.location.reload();
+      } else {
+        localStorage.setItem("loggedIn", "false");
+        localStorage.removeItem("loggedUser");
+        window.location.reload();
+      }
     }
+  }
 
-    if (loggedUser && loggedUser.email !== null) {
-        let account = document.getElementById("cuenta");
-        account.removeAttribute("href");
-        account.innerHTML = `
-        <div class="col-md-6 col-lg-6">
-        <div id="dropdown" class="dropdown">
+  if (loggedUser && loggedUser.email !== null) {
+    let account = document.getElementById("cuenta");
+    account.removeAttribute("href");
+    account.innerHTML = `
+        <button id="dropdown" class="dropdown btn-dark">
             ${loggedUser.email} 
-            <ul id="dropdown-contain">
-                <li class="dropdown-item"><a href="my-profile.html">Mi perfil</a></li>
-                <li class="dropdown-item"><a href="cart.html">Carrito</a></li>
-                <li class="dropdown-item" id="cerrarSesion">Cerrar sesión</li>
-            </ul>
-        </div>
-    </div>
+    <ul id="dropdown-contain" class="text-start btn">
+        <li class="dropdown-item"><a href="my-profile.html">Mi perfil</a></li>
+        <li class="dropdown-item"><a href="cart.html">Carrito</a></li>
+        <li class="dropdown-item" id="cerrarSesion">Cerrar sesión</li>
+    </ul>
+
+        </button>
         `;
-    }
+  }
 
-    if (window.location.pathname.includes("login.html")) {
-    } else if ( !rememberMeCookie || localStorage.getItem("loggedIn") !== "true") {
-        setTimeout(function () {
-            window.location.href = "login.html";
-        }, 2000);
-    }
+  if (window.location.pathname.includes("login.html")) {
+  } else if (!rememberMeCookie || localStorage.getItem("loggedIn") !== "true") {
+    setTimeout(function () {
+      window.location.href = "login.html";
+    }, 2000);
+  }
 
+  let logOut = document.getElementById("cerrarSesion");
 
-    let logOut = document.getElementById("cerrarSesion");
-
-    if (logOut) {
-        logOut.addEventListener("click", function (e) {
-            localStorage.removeItem("loggedUser");
-            localStorage.removeItem("loggedIn");
-            console.log(loggedUser);
-            e.stopPropagation()
-            deleteCookie("rememberMe");
-            window.location.reload();
-        });
-    }
-})
+  if (logOut) {
+    logOut.addEventListener("click", function (e) {
+      localStorage.removeItem("loggedUser");
+      localStorage.removeItem("loggedIn");
+      console.log(loggedUser);
+      e.stopPropagation();
+      deleteCookie("rememberMe");
+      window.location.reload();
+    });
+  }
+});
